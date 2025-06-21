2933eae4523df66bedc7f3b7a6054cabb3ed2da8
Carlos Rocha <carlosrocha0201@gmail.com>
2025-06-21 09:13:08 -0300

    Dasafio Concluídogit add  Models/Iphone.cs  Models/Nokia.cs Models/Smartphone.cs  Program.csgit add  Models/Iphone.cs  Models/Nokia.cs Models/Smartphone.cs  Program.cs!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia (numero: "9999-8888",modelo: "básico", imei:"111100000",memoria: 65);
+nokia.Ligar();
+nokia.InstalarAplicativo("dotnet");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone (numero: "5555-7777",modelo: "médio", imei:"222223333",memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("vccode");
